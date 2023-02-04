88cd31f09e542a84717745376206818fcfb3b3ef
Prasanth Sekar <prasanth.sekar@sisainfosec.com>
2023-02-05 01:07:00 +0530

    feature animation added

---
+++ src/app/interface/about.interface.ts
@@ -0,0 +1,7 @@
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+export interface iAbout {
+    icon: IconDefinition;
+    name: string;
+    description: string;
+}
--- src/app/view/features/features.component.ts
+++ src/app/view/features/features.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shardible-features',
   templateUrl: './features.component.html',
   styleUrls: ['./features.component.scss']
 })
-export class FeaturesComponent implements OnInit {
+export class FeaturesComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    document.querySelector('menuBtn')?.classList.toggle('open');
   }
 
 }
